4f94b4662c2e2d169d64c61cce0ef1b1fad646d7
CuriousDeveloperMom <prit.solanki@gmail.com>
2021-09-21 12:24:58 -0400

    Changed app.ts to server.ts and set up basic express server

---
+++ server.ts
@@ -0,0 +1,17 @@
+import express, { Application, Request, Response, NextFunction } from 'express';
+
+const app: Application = express();
+const PORT = process.env.PORT || 5000;
+
+app.get('/', (req: Request, res: Response) => {
+  //next: NextFunction
+  console.log('TEST!!');
+  res.send('Hello from Express Server!!');
+});
+
+app.listen(PORT, () =>
+  console.log(`Server Running on Port : http://localhost:${PORT}`)
+);
+
+// const greeting: string ='Hello';
+// const numbers: number[] = [1,2,3];
